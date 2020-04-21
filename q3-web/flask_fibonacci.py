2ff1e7c07451c9fe6f848fa9ebf8f0d797c313ec
Rahian Islam <rahian.islam1@ucalgary.ca>
2020-04-21 10:47:04 -0600

    Just finished Question 3 but stil confused about a question so emailed

--- q3-web/flask_fibonacci.py
+++ q3-web/flask_fibonacci.py
@@ -1,1 +1,38 @@
-# Author: <your name>
+# Author: Rahian Islam
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ('Welcome to the Fibonacci Library')
+
+
+@app.route('/fibonacci')
+def fib():
+    return ('Please enter the number of elements you want to see')
+
+
+@app.route('/fibonacci/<num>')
+def fibonacci(num): 
+    num = int(num)
+    if num<0: 
+        print("Incorrect input") 
+  
+    else:
+        a = 0
+        b = 1
+        c = 1
+        fibo=[b]
+        while b < 70 and c<num:
+            a, b = b, a+b
+            fibo.append(b)
+            c += 1 
+        
+        return render_template('fibonacci.html', num_elements = num, fib_num = fibo) 
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
