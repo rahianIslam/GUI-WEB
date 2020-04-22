89dc147af48e8d10a85690405a052c360bc02a75
Rahian Islam <rahian.islam1@ucalgary.ca>
2020-04-21 19:38:23 -0600

    finally finished the question 1 but i get an error whichi have to fix

--- q1-cli/convert_covid_csv2sqlite.py
+++ q1-cli/convert_covid_csv2sqlite.py
@@ -2,21 +2,25 @@
 import argparse
 import pandas as pd
 from sqlalchemy import create_engine
+import sqlite3
 # TODO: add additional imports
 
 def convert_csv2sqlite(csv_name, sqlite_name, drop_last_n=0):
     csv = pd.read_csv(csv_name)
+    daily_cases = csv.pivot_table(index=['Date reported'], aggfunc='size')
+    data_till_drop = daily_cases[0:drop_last_n]
+    # dates = csv['Date reported']
+    print(sqlite_name)
 
-    db_uri = 'sqlite:///measurements.db'
+    db_uri = 'sqlite:///measurements2.db'
     engine = create_engine(db_uri, echo=False)
-    csv.to_sql(name='sqlite_name', con=engine, if_exists='replace') 
+    data_till_drop.to_sql(name=sqlite_name, con=engine, if_exists='replace')
+    engine.execute("SELECT * FROM sqlite_name")
     engine.dispose()
-    print (csv)
-    #TODO: implement
+    
+    
 
-# def csv_read ():
-#     # csv = pd.read_csv('csv_name')
-#     print('abcd')
+
     
 
 parser = argparse.ArgumentParser(description='Converting covid19stats.alberta.ca csv file to custom sqlite db')
@@ -27,8 +31,8 @@ parser.add_argument('-d','--drop', help = 'Specify how many rows to drop')
 
 
 args = parser.parse_args()
-
-convert_csv2sqlite(args.csv_filename, args.sqlite_filename, drop_last_n=args.drop)
+dropn = int(args.drop)
+convert_csv2sqlite(args.csv_filename, args.sqlite_filename, drop_last_n=dropn)
 
 
 # taek into pandas data frame then covert
--- q1-cli/test.py
+++ q1-cli/test.py
@@ -1,14 +1,27 @@
 import pandas as pd
+import sqlite3
+
+def count(n):
+
+    csv = pd.read_csv('covid19dataexport.csv')   # reading the file 
+    b = csv.pivot_table(index=['Date reported'], aggfunc='size')  # converts to number of casess pper day
+    b.columns = ['Date']
+    drop = b[0:n]
+    print(drop)
+
+# b = csv['Date reported']
 
 
-count = 0
-
-csv = pd.read_csv('covid19dataexport.csv')
-
-b = csv['Date reported']
-c = csv[csv.duplicated(['Date reported'])]
-for i in c:
-    if c[i] in b:
-        count += 1
+# c = csv[csv.duplicated(['Date reported'])]
+# for i in c:
+#     if c[i] in b:
+#         count += 1
     
-print(count)
+# print(c)
+# print([0])
+count(5)
+
+conn = sqlite3.connect('measurements2.db')
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM alberta")
